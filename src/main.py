892529ba73ca797db69dafa06529841ce71f7e6b
mrvictoru <pingumacao@gmail.com>
2023-08-26 11:20:10 +0800

    testing

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ def main():
     print("Check PyTorch version and GPU availability:")
     print(torch.__version__)
     print(torch.cuda.is_available())
+    print("bye world")
 
 if __name__ == "__main__":
     main()

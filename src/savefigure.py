e2980e706459eaef3b79241be31ac2d946ead273
mrvictoru <pingumacao@gmail.com>
2023-09-27 17:47:14 +0800

    added save2gif function

--- src/savefigure.py
+++ src/savefigure.py
@@ -2,6 +2,7 @@
 
 import os
 import matplotlib.pyplot as plt
+import imageio
 
 def save_figures(figures, directory):
     # check if the directory exists
@@ -16,3 +17,19 @@ def save_figures(figures, directory):
         plt.imsave(f"{directory}/{step}.png", figure)
         plt.close()
         print(step, "saved")
+
+def save_gif(directory, gif_name):
+    # get the list of file names
+    filenames = os.listdir(directory)
+    # exclude the non-png files
+    filenames = [filename for filename in filenames if filename.endswith('.png')]
+    # sort the file names
+    filenames = sorted(filenames)
+    # create the list of file paths
+    filenames = [directory + '/' + filename for filename in filenames]
+    # create the list of images
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread(filename))
+    # save the gif
+    imageio.mimsave(gif_name, images, duration=0.2)

e49a0139f651b32cc84c766ebb3bbb121e589f15
mrvictoru <pingumacao@gmail.com>
2023-09-29 12:11:43 +0800

    added makegymenv

--- src/curatedataset.py
+++ src/curatedataset.py
@@ -1,1 +1,43 @@
 # This script is to get stock data, create a gym environment from data, create agent to trade in the envrionment and collect trading data and save as dataset
+
+# first group of functions are to get stock data
+
+# import helper functions for getting stock data
+from getstock import get_stock_data_yf_between_with_indicators
+# import datatime library
+from datetime import datetime, timedelta
+
+from TradingEnvClass import StockTradingEnv
+
+import numpy as np
+
+# start_date need to be in format of 'YYYY-MM-DD'
+def makegymenv(stock_name, start_date, period, interval='1d', indicators=['Volume', 'volume_cmf', 'trend_macd', 'momentum_rsi'], init_balance = 20000, random = False):
+    # work out the end_date from start_date and period
+    try:
+        start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date_obj = start_date_obj + timedelta(days=period)
+        end_date = end_date_obj.strftime('%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+        
+    stock_data = get_stock_data_yf_between_with_indicators(stock_name, start_date, end_date, interval, indicators)
+
+    # loop through the data and check for any NaN values or inf values
+    infnancheck = False
+    for col in stock_data.columns:
+        if stock_data[col].isnull().values.any() or np.isinf(stock_data[col]).values.any():
+            print(f'NaN or inf value found in {col}')
+            infnancheck = True
+    if infnancheck:
+        raise ValueError("NaN or inf value found in stock data")
+    
+    # create gym environment
+    max_step = len(stock_data) - 1
+    env = StockTradingEnv(stock_data, init_balance, max_step, random)
+
+    return env
+
+        
+
+    
